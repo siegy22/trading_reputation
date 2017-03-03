905228e9be5d42821f140ee939315ba2de26197d
Yves Siegrist <Elektron1c97@gmail.com>
2017-03-03 22:52:16 +0100

    add bootstrap and choose bootswatch paper theme

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
-  def home
+  def show
+    public_send(params[:page]) if action_methods.include?(params[:page])
+    page_path = "pages/#{params[:page]}"
+    if template_exists?(page_path)
+      render page_path
+    else
+      render_404
+    end
   end
 end
